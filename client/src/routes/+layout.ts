277e1ac3f0305351019e099493c1137a4044e65e
myin <minyin142p@gmail.com>
2025-12-26 14:42:30 +0100

    add svelte client

---
+++ client/src/routes/+layout.ts
@@ -0,0 +1,4 @@
+// This tells SvelteKit to prerender all pages at build time
+// and generate static HTML files
+export const prerender = true;
+export const ssr = false; // Disable server-side rendering for a pure SPA
