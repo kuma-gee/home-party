dd7ccc4c97daf4b70d183430870aa494fd9b1c2a
myin <minyin142p@gmail.com>
2026-05-28 23:42:43 +0200

    fix desktop gate hp

--- mods-unpacked/KumaGee-VRCore/castle-defense/health_bar.gd
+++ mods-unpacked/KumaGee-VRCore/castle-defense/health_bar.gd
@@ -3,9 +3,8 @@ extends ProgressBar
 @export var hurtbox: HurtBox
 
 func _ready() -> void:
-	await hurtbox.ready
-	hurtbox.health_changed.connect(_update)
-	
+	if not hurtbox.health_changed.is_connected(_update):
+		hurtbox.health_changed.connect(_update)
 	max_value = hurtbox.health
 	_update()
 
