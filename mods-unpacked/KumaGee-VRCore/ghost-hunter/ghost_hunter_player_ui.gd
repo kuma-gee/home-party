8f4f0f201ca85dc741ac80ca7116cd218efd83d1
myin <minyin142p@gmail.com>
2026-06-15 18:56:54 +0200

    remove ghost hunter

--- mods-unpacked/KumaGee-VRCore/ghost-hunter/ghost_hunter_player_ui.gd
+++
@@ -1,9 +0,0 @@
-class_name GhostHunterPlayerUI
-extends JoinedPlayer
-
-
-func _ready() -> void:
-	super()
-	# TODO: Send the appropriate layout for this game's mobile UI
-	# LobbyServer.send_layout("ghost_hunter")
-	# TODO: Connect game-specific callbacks (word submit, guess input, etc.)
