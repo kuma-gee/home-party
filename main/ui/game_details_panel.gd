38350aa00104a14e0b1201d33eca63925ce7436f
myin <minyin142p@gmail.com>
2026-06-26 22:13:26 +0200

    fixes?

--- main/ui/game_details_panel.gd
+++ main/ui/game_details_panel.gd
@@ -6,6 +6,9 @@ extends Control
 @export var player_label: Label
 @export var bg_image: TextureRect
 
+func _ready() -> void:
+	pass
+
 func update_details(game: GameResource) -> void:
 	if not game:
 		hide()
@@ -19,5 +22,5 @@ func update_details(game: GameResource) -> void:
 	else:
 		player_label.text = "👤 %d–%d players" % [game.min_recommended_players, game.max_recommended_players]
 
-	bg_image.texture = game.vr_preview
+	#bg_image.texture = game.vr_preview
 	show()
